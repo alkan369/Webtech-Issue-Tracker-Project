5c51f0b531b08b6a62bb6ac88f569c6fafc882be
Daniel <dd01@abv.bg>
2023-06-16 21:16:21 +0300

    connected frontend to backend

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 import express, { Application, json } from "express";
 import { ConnectOptions, connect } from 'mongoose';
 import { connect as connectAPI } from "./api/connect";
+import path from 'path';
+import bodyParser from 'body-parser';
 
 config()
 
@@ -13,6 +15,12 @@ connectAPI(app, '/api');
 
 const PORT = process.env.PORT || 3001;
 const DB_CONNECTION = process.env.DB_CONNECTION_STRING;
+
+app.use(express.static(path.join(__dirname, '../../issue-tracker/build')));
+app.use(bodyParser.json());
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../issue-tracker/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     connect(DB_CONNECTION as string, {
