090cbfc0f30bd1f08005277b316423239075cab9
Daniel <dd01@abv.bg>
2023-06-17 12:06:58 +0300

    separated the form functions from the html form, added navigation

---
+++ issue-tracker/src/components/welcome-page/FormFunctions.js
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+export function useFormState() {
+  const [regPwdEyeClass, setRegPwdEyeClass] = useState(faEye);
+  const [confirmPwdEyeClass, setConfirmPwdEyeClass] = useState(faEye);
+  const [loginPwdEyeClass, setLoginPwdEyeClass] = useState(faEye);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  return {
+    regPwdEyeClass,
+    setRegPwdEyeClass,
+    confirmPwdEyeClass,
+    setConfirmPwdEyeClass,
+    loginPwdEyeClass,
+    setLoginPwdEyeClass,
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    username,
+    setUsername,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confirmPassword,
+    setConfirmPassword,
+    loginUsername,
+    setLoginUsername,
+    loginPassword,
+    setLoginPassword,
+  };
+}
+
+export function handleRegistration(navigate, registrationData) {
+  axios
+    .post('/api/users/create', registrationData)
+    .then((response) => {
+      alert('Welcome to IssueTracker!');
+      navigate('/tickets');
+    })
+    .catch((error) => {
+      alert(error.response.data.message);
+    });
+}
+
+export function handleLogin(navigate, loginData) {
+  axios
+    .post('/api/users/login', loginData)
+    .then((response) => {
+      alert(`Welcome back ${loginData.loginUsername}!`);
+      navigate('/tickets');
+    })
+    .catch((error) => {
+      alert(error.response.data.message);
+    });
+}
+
+export function handleTogglePasswordVisibility(inputId, setEyeClass) {
+  if (inputId === 'reg_password') {
+    setEyeClass((prevClass) =>
+      prevClass === faEye ? faEyeSlash : faEye
+    );
+  } else if (inputId === 'confirm_password') {
+    setEyeClass((prevClass) =>
+      prevClass === faEye ? faEyeSlash : faEye
+    );
+  } else if (inputId === 'login_password') {
+    setEyeClass((prevClass) =>
+      prevClass === faEye ? faEyeSlash : faEye
+    );
+  }
+
+  const input = document.getElementById(inputId);
+  const inputType = input.getAttribute('type') === 'password' ? 'text' : 'password';
+  input.setAttribute('type', inputType);
+}
+
+export function handleLoginVisibility() {
+  const loginBox = document.getElementById('login-box');
+  loginBox.classList.toggle('show');
+}
--- issue-tracker/src/components/welcome-page/Welcome.js
+++ issue-tracker/src/components/welcome-page/Welcome.js
@@ -1,86 +1,34 @@
 import './Welcome.css';
 import logo from './logo.png';
-import { useState } from 'react';
-import axios from 'axios';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate } from 'react-router-dom';
+import { useFormState, handleRegistration, handleLogin, handleTogglePasswordVisibility, handleLoginVisibility } from './FormFunctions';
+
 
 function App() {
-  const [regPwdEyeClass, setRegPwdEyeClass] = useState(faEye);
-  const [confirmPwdEyeClass, setConfirmPwdEyeClass] = useState(faEye);
-  const [loginPwdEyeClass, setLoginPwdEyeClass] = useState(faEye);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+  const formState = useFormState();
 
-  const [loginUsername, setLoginUsername] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
-
-
-  const handleRegistration = () => {
-    // Perform input validation and other checks
-
+  const registration = () => {
     const registrationData = {
-      firstName,
-      lastName,
-      username,
-      email,
-      password,
-      confirmPassword
+      firstName: formState.firstName,
+      lastName: formState.lastName,
+      username: formState.username,
+      email: formState.email,
+      password: formState.password,
+      confirmPassword: formState.confirmPassword,
     };
 
-    axios
-      .post('/api/users/create', registrationData)
-      .then((response) => {
-        alert('Welcome to IssueTracker!');
-      })
-      .catch((error) => {
-        alert(error.response.data.message);
-      });
+    handleRegistration(navigate, registrationData);
   };
 
-  const handleLogin = () => {
+  const login = () => {
     const loginData = {
-      loginUsername,
-      loginPassword,
+      loginUsername: formState.loginUsername,
+      loginPassword: formState.loginPassword,
     };
 
-    axios
-      .post('/api/users/login', loginData)
-      .then((response) => {
-        alert(`Welcome back ${loginUsername}!`);
-      })
-      .catch((error) => {
-        alert(error.response.data.message);
-      });
-  };
-
-  const handleTogglePasswordVisibility = (inputId) => {
-    if (inputId === 'reg_password') {
-      setRegPwdEyeClass((prevClass) =>
-        prevClass === faEye ? faEyeSlash : faEye
-      );
-    } else if (inputId === 'confirm_password') {
-      setConfirmPwdEyeClass((prevClass) =>
-        prevClass === faEye ? faEyeSlash : faEye
-      );
-    } else if (inputId === 'login_password') {
-      setLoginPwdEyeClass((prevClass) =>
-        prevClass === faEye ? faEyeSlash : faEye
-      );
-    }
-    const input = document.getElementById(inputId);
-    const inputType =
-      input.getAttribute('type') === 'password' ? 'text' : 'password';
-    input.setAttribute('type', inputType);
-  };
-
-  const handleLoginVisibility = () => {
-    const loginBox = document.getElementById('login-box');
-    loginBox.classList.toggle('show');
+    handleLogin(navigate, loginData);
   };
 
   return (
@@ -89,30 +37,30 @@ function App() {
         <input type="checkbox" id="chk" aria-hidden="true" />
         <div className="register">
             <label className='welcome-label' htmlFor="chk" aria-hidden="true" onClick={() => handleLoginVisibility()}>Register</label>
-            <input className='welcome-input' type="text" id="fname" name="txt" placeholder="First Name" required onChange={(e) => setFirstName(e.target.value)} value={firstName} />
-            <input className='welcome-input' type="text" id="lname" name="txt" placeholder="Last Name" required onChange={(e) => setLastName(e.target.value)} value={lastName} />
-            <input className='welcome-input' type="text" name="txt" id="reg_username" placeholder="Username" required onChange={(e) => setUsername(e.target.value)} value={username} />
-            <input className='welcome-input' type="email" name="email" id="reg_email" placeholder="Email" required onChange={(e) => setEmail(e.target.value)} value={email} />
+            <input className='welcome-input' type="text" id="fname" name="txt" placeholder="First Name" required onChange={(e) => formState.setFirstName(e.target.value)} value={formState.firstName} />
+            <input className='welcome-input' type="text" id="lname" name="txt" placeholder="Last Name" required onChange={(e) => formState.setLastName(e.target.value)} value={formState.lastName} />
+            <input className='welcome-input' type="text" name="txt" id="reg_username" placeholder="Username" required onChange={(e) => formState.setUsername(e.target.value)} value={formState.username} />
+            <input className='welcome-input' type="email" name="email" id="reg_email" placeholder="Email" required onChange={(e) => formState.setEmail(e.target.value)} value={formState.email} />
             <div className="password-container">
-              <input className='welcome-input' type="password" name="pwd" id="reg_password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} value={password} />
-              <FontAwesomeIcon icon={regPwdEyeClass} id="eye1" onClick={() => handleTogglePasswordVisibility('reg_password')} />
+              <input className='welcome-input' type="password" name="pwd" id="reg_password" placeholder="Password" required onChange={(e) => formState.setPassword(e.target.value)} value={formState.password} />
+              <FontAwesomeIcon icon={formState.regPwdEyeClass} id="eye1" onClick={() => handleTogglePasswordVisibility('reg_password')} />
             </div>
             <div className="password-container">
-              <input className='welcome-input' type="password" name="cpwd" id="confirm_password" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
-              <FontAwesomeIcon icon={confirmPwdEyeClass} id="eye2" onClick={() => handleTogglePasswordVisibility('confirm_password')} />
+              <input className='welcome-input' type="password" name="cpwd" id="confirm_password" placeholder="Confirm Password" onChange={(e) => formState.setConfirmPassword(e.target.value)} value={formState.confirmPassword} />
+              <FontAwesomeIcon icon={formState.confirmPwdEyeClass} id="eye2" onClick={() => handleTogglePasswordVisibility('confirm_password')} />
             </div>
-            <button className='welcome-button' id="regBtn" onClick={handleRegistration}>Register</button>
+            <button className='welcome-button' id="regBtn" onClick={registration}>Register</button>
         </div>
 
         <div className="login">
           <label className='welcome-label' htmlFor="chk" aria-hidden="true" onClick={() => handleLoginVisibility()}>Login</label>
           <div id="login-box">
-            <input className='welcome-input' type="text" name="txt" id="login_username" placeholder="Username" required onChange={(e) => setLoginUsername(e.target.value)} value={loginUsername} />
+            <input className='welcome-input' type="text" name="txt" id="login_username" placeholder="Username" required onChange={(e) => formState.setLoginUsername(e.target.value)} value={formState.loginUsername} />
             <div className="password-container">
-              <input className='welcome-input' type="password" name="pwd" id="login_password" placeholder="Password" required onChange={(e) => setLoginPassword(e.target.value)} value={loginPassword} />
-              <FontAwesomeIcon icon={loginPwdEyeClass} id="eye3" onClick={() => handleTogglePasswordVisibility('login_password')} />
+              <input className='welcome-input' type="password" name="pwd" id="login_password" placeholder="Password" required onChange={(e) => formState.setLoginPassword(e.target.value)} value={formState.loginPassword} />
+              <FontAwesomeIcon icon={formState.loginPwdEyeClass} id="eye3" onClick={() => handleTogglePasswordVisibility('login_password')} />
             </div>
-            <button className='welcome-button' id="logBtn" onClick={handleLogin}>Login</button>
+            <button className='welcome-button' id="logBtn" onClick={login}>Login</button>
             <img src={logo} alt="Logo" />
           </div>
         </div>
