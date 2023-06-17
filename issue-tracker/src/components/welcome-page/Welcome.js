bafd3d9253b396bd79a82813fad7b253da310a61
Daniel <dd01@abv.bg>
2023-06-17 12:56:26 +0300

    changed file structure

--- issue-tracker/src/components/welcome-page/Welcome.js
+++ issue-tracker/src/components/welcome-page/Welcome.js
@@ -34,34 +34,146 @@ function App() {
   return (
     <div className="body-welcome">
       <div className="main">
-        <input type="checkbox" id="chk" aria-hidden="true" />
+        <input 
+          type="checkbox" 
+          id="chk" 
+          aria-hidden="true" 
+        />
         <div className="register">
-            <label className='welcome-label' htmlFor="chk" aria-hidden="true" onClick={() => handleLoginVisibility()}>Register</label>
-            <input className='welcome-input' type="text" id="fname" name="txt" placeholder="First Name" required onChange={(e) => formState.setFirstName(e.target.value)} value={formState.firstName} />
-            <input className='welcome-input' type="text" id="lname" name="txt" placeholder="Last Name" required onChange={(e) => formState.setLastName(e.target.value)} value={formState.lastName} />
-            <input className='welcome-input' type="text" name="txt" id="reg_username" placeholder="Username" required onChange={(e) => formState.setUsername(e.target.value)} value={formState.username} />
-            <input className='welcome-input' type="email" name="email" id="reg_email" placeholder="Email" required onChange={(e) => formState.setEmail(e.target.value)} value={formState.email} />
+            <label 
+              className='welcome-label' 
+              htmlFor="chk" 
+              aria-hidden="true" 
+              onClick={() => handleLoginVisibility()}>
+              Register
+            </label>
+            <input 
+              className='welcome-input' 
+              type="text" 
+              id="fname" 
+              name="txt" 
+              placeholder="First Name" 
+              required 
+              onChange={(e) => formState.setFirstName(e.target.value)} 
+              value={formState.firstName} 
+            />
+            <input 
+              className='welcome-input' 
+              type="text" 
+              id="lname" 
+              name="txt" 
+              placeholder="Last Name" 
+              required 
+              onChange={(e) => formState.setLastName(e.target.value)} 
+              value={formState.lastName} 
+            />
+            <input 
+              className='welcome-input' 
+              type="text" 
+              name="txt" 
+              id="reg_username" 
+              placeholder="Username" 
+              required 
+              onChange={(e) => formState.setUsername(e.target.value)} 
+              value={formState.username} 
+            />
+            <input 
+              className='welcome-input' 
+              type="email" 
+              name="email" 
+              id="reg_email" 
+              placeholder="Email" 
+              required 
+              onChange={(e) => formState.setEmail(e.target.value)} 
+              value={formState.email} 
+            />
             <div className="password-container">
-              <input className='welcome-input' type="password" name="pwd" id="reg_password" placeholder="Password" required onChange={(e) => formState.setPassword(e.target.value)} value={formState.password} />
-              <FontAwesomeIcon icon={formState.regPwdEyeClass} id="eye1" onClick={() => handleTogglePasswordVisibility('reg_password', formState.setRegPwdEyeClass)} />
+              <input 
+                className='welcome-input' 
+                type="password" 
+                name="pwd" 
+                id="reg_password" 
+                placeholder="Password" 
+                required 
+                onChange={(e) => formState.setPassword(e.target.value)} 
+                value={formState.password} 
+              />
+              <FontAwesomeIcon 
+                icon={formState.regPwdEyeClass} 
+                id="eye1" 
+                onClick={() => handleTogglePasswordVisibility('reg_password', formState.setRegPwdEyeClass)} 
+              />
             </div>
             <div className="password-container">
-              <input className='welcome-input' type="password" name="cpwd" id="confirm_password" placeholder="Confirm Password" onChange={(e) => formState.setConfirmPassword(e.target.value)} value={formState.confirmPassword} />
-              <FontAwesomeIcon icon={formState.confirmPwdEyeClass} id="eye2" onClick={() => handleTogglePasswordVisibility('confirm_password', formState.setConfirmPwdEyeClass)} />
+              <input 
+                className='welcome-input' 
+                type="password" 
+                name="cpwd" 
+                id="confirm_password" 
+                placeholder="Confirm Password" 
+                onChange={(e) => formState.setConfirmPassword(e.target.value)} 
+                value={formState.confirmPassword} 
+              />
+              <FontAwesomeIcon 
+              icon={formState.confirmPwdEyeClass} 
+              id="eye2" 
+              onClick={() => handleTogglePasswordVisibility('confirm_password', formState.setConfirmPwdEyeClass)} 
+            />
             </div>
-            <button className='welcome-button' id="regBtn" onClick={registration}>Register</button>
+            <button 
+              className='welcome-button' 
+              id="regBtn" 
+              onClick={registration}>
+              Register
+            </button>
         </div>
 
         <div className="login">
-          <label className='welcome-label' htmlFor="chk" aria-hidden="true" onClick={() => handleLoginVisibility()}>Login</label>
+          <label 
+            className='welcome-label' 
+            htmlFor="chk" 
+            aria-hidden="true" 
+            onClick={() => handleLoginVisibility()}>
+            Login
+          </label>
           <div id="login-box">
-            <input className='welcome-input' type="text" name="txt" id="login_username" placeholder="Username" required onChange={(e) => formState.setLoginUsername(e.target.value)} value={formState.loginUsername} />
+            <input 
+              className='welcome-input' 
+              type="text" 
+              name="txt" 
+              id="login_username" 
+              placeholder="Username" 
+              required 
+              onChange={(e) => formState.setLoginUsername(e.target.value)} 
+              value={formState.loginUsername} 
+            />
             <div className="password-container">
-              <input className='welcome-input' type="password" name="pwd" id="login_password" placeholder="Password" required onChange={(e) => formState.setLoginPassword(e.target.value)} value={formState.loginPassword} />
-              <FontAwesomeIcon icon={formState.loginPwdEyeClass} id="eye3" onClick={() => handleTogglePasswordVisibility('login_password', formState.setLoginPwdEyeClass)} />
+              <input 
+                className='welcome-input' 
+                type="password" 
+                name="pwd" 
+                id="login_password" 
+                placeholder="Password" 
+                required 
+                onChange={(e) => formState.setLoginPassword(e.target.value)} 
+                value={formState.loginPassword} 
+              />
+              <FontAwesomeIcon 
+                icon={formState.loginPwdEyeClass} 
+                id="eye3" 
+                onClick={() => handleTogglePasswordVisibility('login_password', formState.setLoginPwdEyeClass)} 
+              />
             </div>
-            <button className='welcome-button' id="logBtn" onClick={login}>Login</button>
-            <img src={logo} alt="Logo" />
+            <button 
+              className='welcome-button' 
+              id="logBtn" 
+              onClick={login}>
+              Login
+            </button>
+            <img 
+              src={logo} 
+              alt="Logo" 
+            />
           </div>
         </div>
       </div>
