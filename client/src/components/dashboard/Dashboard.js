d25f73cca502774307caa76c062b23f76ef2ae86
Yavor Bojinov <72650953+yavor0805@users.noreply.github.com>
2023-06-22 19:33:21 +0300

    Added Logout functionality

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import logo from '../welcome-page/logo.png';
-import Tickets from '../tickets-page/Tickets';
-import Projects from '../projects/Projects';
-import './Dashboard.css';
+import React from "react";
+import logo from "../welcome-page/logo.png";
+import Tickets from "../tickets-page/Tickets";
+import Projects from "../projects/Projects";
+import "./Dashboard.css";
+import { useNavigate } from "react-router-dom";
+
 const App = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    //Hangle token deletion from local/session storage
+    navigate("/");
+  };
+
   return (
     <div>
       <header className="ticket-header">
-        <img src={logo} alt="Logo" style={{ float: 'left', height: '80px', width: '90px', backgroundColor: '#fcfcfc' }} />
-        <h1 style={{ marginLeft: '60px' }}>Issue Tracker</h1>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ float: "left", height: "80px", width: "90px", backgroundColor: "#fcfcfc" }}
+          />
+          <h1 style={{ marginLeft: "60px", marginRight: "auto" }}>Issue Tracker</h1>
+          <button onClick={handleLogout} className="delete-button" style={{ maxHeight: "4rem" }}>
+            Logout
+          </button>
+        </div>
       </header>
       <main>
         <Tickets />
